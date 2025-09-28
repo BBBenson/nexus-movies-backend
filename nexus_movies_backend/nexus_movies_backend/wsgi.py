84c9c1afc754a332e827178e9e8ebd16d917a1c6
BBBenson <bbbenson26@gmail.com>
2025-09-28 05:58:14 +0000

    update settings

--- nexus_movies_backend/nexus_movies_backend/wsgi.py
+++ nexus_movies_backend/nexus_movies_backend/wsgi.py
@@ -2,5 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus_movies_backend.settings')
-
-application = get_wsgi_application
+application = get_wsgi_application()
